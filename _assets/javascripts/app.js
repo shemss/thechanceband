1d3d4f8e1de6c3781cc2d9ee62a022640efdb086
Aleksey Gladysh <aleksey.gladysh@gmail.com>
2016-03-12 17:26:37 -0800

    Add video preview code for gallery

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,6 +1,7 @@
 //= require vendor/jquery-1.11.1
 //= require vendor/jquery.scrollTo
 //= require vendor/jquery.fancybox
+//= require vendor/jquery.fancybox-media
 //= require vendor/jquery.cookie
 //= require vendor/twitterFetcher
 //= require vendor/autoplay-detection
